77da37ac4960762064c17cd49b8b74715cc583e2
Andrew Effendi <andrewamadeo20@gmail.com>
2021-05-19 10:00:35 +0700

    added comments to some file

--- src/com/chess/Piece.java
+++ src/com/chess/Piece.java
@@ -14,6 +14,8 @@ public class Piece {
     boolean is_w;
     LinkedList<Piece> pcs;
     String name;
+
+    // this object is used to store all the data of the related chess piece
     public Piece(String pcs_name, boolean is_w, int gridX, int gridY, LinkedList<Piece> pcs) {
         name = pcs_name;
         this.is_w = is_w;
@@ -25,6 +27,7 @@ public class Piece {
         pcs.add(this);
     }
 
+    //this method is used to determine the movement of each pieces
     public boolean move(int gridX,int gridY,int current_Player){
         //System.out.println(ChessGame.getPiece(this.gridX*64, this.gridY*64).name);
         //check if its player's turn
@@ -50,10 +53,12 @@ public class Piece {
         return false;
     }
 
+    //this method is used to kill the chess pieces i.e. remove it from the board
     public void kill(){
         pcs.remove(this);
     }
 
+    //this method is used to move the piece to the new grid and kill the opponent on that grid if there is any
     public boolean moveToKill(int gridX,int gridY){
         if(Main.getPiece(gridX*64, gridY*64)!=null){
             if(Main.getPiece(gridX*64, gridY*64).is_w!=is_w){
@@ -69,13 +74,16 @@ public class Piece {
         return true;
     }
 
+    //this method is used for invalid movement and do nothing so the move wouldn't be registered to the board
     public boolean doNothing(){
         x=this.gridX*64;
         y=this.gridY*64;
         return false;
     }
 
+    //this method is used to determine the movement for the pawn
     public boolean pawnMove(int gridX,int gridY){
+        //for white pieces
         if(this.is_w){
             //kill
             if(Main.getPiece(gridX*64, gridY*64)!=null && this.gridY - gridY == 1 && (this.gridX - gridX == 1 || this.gridX - gridX == -1 ) && !Main.getPiece(gridX*64, gridY*64).is_w){
@@ -86,6 +94,7 @@ public class Piece {
                 return doNothing();
             }
         }
+        //for black pieces
         else{
             //kill
             if(Main.getPiece(gridX*64, gridY*64)!=null && this.gridY - gridY == -1 && (this.gridX - gridX == 1 || this.gridX - gridX == -1 ) && Main.getPiece(gridX*64, gridY*64).is_w){
@@ -99,12 +108,14 @@ public class Piece {
         }
         return moveToKill(gridX, gridY);
     }
+
+    // this is the method that determines the movement for the bishop
     public boolean bishopMove(int gridX,int gridY){
         //fail move not diagonal
         if(!(this.gridX - gridX == this.gridY - gridY || this.gridX - gridX == - (this.gridY - gridY)) || gridY == this.gridY){
             return doNothing();
         }
-        //fail move have blockade
+        //check if it is a valid diagonal movement
         int i, j,h;
         if(gridY > this.gridY) {
             j = this.gridY + 1;
@@ -118,6 +129,7 @@ public class Piece {
         }
         if((this.gridX - gridX) * (this.gridY - gridY) > 0){
             i++;
+            //checks if there is any blockade along the path
             while (i < h){
                 if(Main.getPiece(i*64, j*64)!=null) return doNothing();
                 i++;
@@ -126,6 +138,7 @@ public class Piece {
         }
         else{
             i--;
+            //checks if there is any blockade along the path
             while (i > h){
                 if(Main.getPiece(i*64, j*64)!=null) return doNothing();
                 i--;
@@ -135,11 +148,13 @@ public class Piece {
         return moveToKill(gridX, gridY);
     }
 
+    //this is the method that determines the movement for the rook
     public boolean rookMove(int gridX,int gridY){
         //fail move not straight
         if(!(this.gridX == gridX || this.gridY == gridY )) return doNothing();
         //fail move have blockade
         int k ,o;
+        //for vertical straight movement
         if(gridX == this.gridX){
             if(gridY > this.gridY){
                 k = this.gridY + 1;
@@ -149,11 +164,14 @@ public class Piece {
                 k = gridY + 1;
                 o = this.gridY;
             }
+            //checks if there is any blockades along the path
             while(k < o){
                 if(Main.getPiece(gridX*64, k*64)!=null) return doNothing();
                 k++;
             }
         }
+
+        //for horizontal straight movement
         else{
             if(gridX > this.gridX){
                 k = this.gridX + 1;
@@ -163,6 +181,7 @@ public class Piece {
                 k = gridX + 1;
                 o = this.gridX;
             }
+            //checks if there is any blockades along the path
             while(k < o){
                 if(Main.getPiece(k*64, gridY*64)!=null) return doNothing();
                 k++;
@@ -170,25 +189,30 @@ public class Piece {
         }
         return moveToKill(gridX, gridY);
     }
+
+    // this is the method that determines the movement for the queen
     public boolean queenMove(int gridX,int gridY){
         //fail move not diagonal nor straight
         if(!(this.gridX - gridX == this.gridY - gridY || this.gridX - gridX == - (this.gridY - gridY) || this.gridX == gridX || this.gridY == gridY)){
             return doNothing();
         }
-        //for straight
+        //checks if it is a valid straight movement
         if(this.gridX == gridX || this.gridY == gridY){
             return rookMove(gridX, gridY);
         }
-        //for diagonal blockade
+        //checks if it si a valid diagonal movement
         return bishopMove(gridX, gridY);
     }
 
+    // this is the method that is used to determines the movement for the king
     public boolean kingMove(int gridX,int gridY){
         if(this.gridX - gridX > 1 || this.gridX - gridX < -1 || this.gridY - gridY > 1 || this.gridY - gridY < -1 ){
             return doNothing();
         }
         return moveToKill(gridX, gridY);
     }
+
+    // this is the method that is used to determine the movement for the knight
     public boolean knightMove(int gridX,int gridY){
         if(!((Math.abs(this.gridX - gridX) == 2 && Math.abs(this.gridY - gridY)==1) || (Math.abs(this.gridX - gridX) == 1 && Math.abs(this.gridY - gridY)==2))){
             return doNothing();
